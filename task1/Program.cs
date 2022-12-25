a8a01d540a97d14fd18c608eff7b1df82df5de55
SvetlanaMedvedeva73 <sweta.medvedev16@yandex.ru>
2022-12-25 16:21:14 +0300

    dz1

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+либо равна 3 символам. Например, “Hello”, “2”, “world”, “:-)”] → [“2”, “:-)” */
+
+Console.Write($"Введите элементы массива: ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+  Console.WriteLine($"Введите {i + 1}-й элемент: ");
+  string element = Convert.ToString(Console.ReadLine());
+  arrayStrings[i] = element;
+}
+ string[] arrayFinal = new string[size];
+ int len = 3;
+ int pos = 0;
+for (int j = 0; j < size; j++)
+     {
+        if (arrayStrings[j].Length <= len)
+        {
+          arrayFinal[pos] = arrayStrings[j];
+          pos++;
+        }
+     }
+
+Console.WriteLine();
+
+
+
+
